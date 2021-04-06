46e78aa469fa19231f4813313a844c32ef9674c6
maliksaepul <malik@inspigo.id>
2021-04-06 14:23:36 +0700

    profile fix routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const Post = require("../../models/Post");
 
-router.get('/', (req, res) => res.json({msg: "user work"}));
+/**
+ * @route GET api/posts
+ * @desc get all post
+ * @access private
+ */
+router.get("/", (req, res) => res.json({ msg: "user work" }));
+
+/**
+ * @route POST api/posts
+ * @desc post all post
+ * @access private
+ */
+router.post("/", passport.authenticate("jwt", { session: false }), 
+(req, res) => {
+    
+}
+);
 
 module.exports = router;
